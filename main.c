b9fdb8505ad5b258aa203d5367bd71dccb06a8c8
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-09 11:57:04 +0530

    Added lsh_launch function

--- main.c
+++ main.c
@@ -5,6 +5,31 @@
 #include <stdio.h>
 #include <string.h>
 
+int lsh_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+ 
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+   
+    perror("lsh");
+  } else {
+    
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
 int lsh_execute(char **args)
 {
   int i;
