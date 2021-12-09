85df05cac2c35b122561443be3bd15655d4adde3
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-09 11:32:42 +0530

    Completed lsh_split_line function

--- main.c
+++ main.c
@@ -69,7 +69,26 @@ char **lsh_split_line(char *line)
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
 
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
 
 
