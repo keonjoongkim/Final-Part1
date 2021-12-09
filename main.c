c9713b40a82517895544c7b33f1963f3b2a51f18
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-09 12:03:20 +0530

    Added lsh_cd function

--- main.c
+++ main.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
 int lsh_help(char **args)
 {
   int i;
