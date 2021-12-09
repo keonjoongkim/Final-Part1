3f0dae27cdc05a266b65336edca3bf5031f98dca
keonjoongkim <89455682+keonjoongkim@users.noreply.github.com>
2021-12-09 11:31:13 +0530

    Started writing lsh_split_line function and added macros

--- main.c
+++ main.c
@@ -56,6 +56,22 @@ char *lsh_read_line(void)
 #endif
 }
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+}
+
 
 void lsh_loop(void)
 {
